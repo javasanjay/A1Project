b2bd20229f6dc2a9002242f082ef21f0a22d824a
kamti <kamti@192.168.1.6>
2018-11-15 02:13:33 +0530

    first  time Commit

---
+++ A1Project/src/A1.java
@@ -0,0 +1,25 @@
+
+
+public class A1 {
+	
+	static int a=m1();
+	
+	static int m1(){
+		System.out.println("ASV");
+		return 10;
+	}
+	static{
+		System.out.println("ASB");
+	}
+
+	/*public static void main(String[] args) {
+		System.out.println("A main");
+    */ 
+
+	}
+
+
+
+
+
+
---
+++ A1Project/src/com/nt/java/A1.java
@@ -0,0 +1,5 @@
+package com.nt.java;
+
+public class A1 {
+
+}
