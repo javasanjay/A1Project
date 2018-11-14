74ddad86b58968e9d60907ba47137f46eb1dc495
kamti <kamti@192.168.1.6>
2018-11-15 02:14:25 +0530

    first time commit

--- A1Project/src/com/nt/java/A1.java
+++ A1Project/src/com/nt/java/A1.java
@@ -1,5 +1,26 @@
 package com.nt.java;
 
-public class A1 {
 
-}
+public class A1 {
+	
+	static int a=m1();
+	
+	static int m1(){
+		System.out.println("ASV");
+		return 10;
+	}
+	static{
+		System.out.println("ASB");
+	}
+
+	/*public static void main(String[] args) {
+		System.out.println("A main");
+    */ 
+
+	}
+
+
+
+
+
+
